3b1a6b2137cd9bf71ba8786349a37447eecb26fb
Chaosborne <rick.chaosborne@gmail.com>
2024-04-09 20:34:07 +0300

    check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
+// Here's the list of users: https://reqres.in/api/users
 
 function App() {
   return (
