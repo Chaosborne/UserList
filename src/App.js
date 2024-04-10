507922f12aa4953e98230b1f489a516a18332ceb
Chaosborne <rick.chaosborne@gmail.com>
2024-04-10 18:49:53 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import { Success } from "./components/Success";
 import { Users } from "./components/Users";
@@ -6,6 +6,21 @@ import { Users } from "./components/Users";
 // Here's the list of users: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.data);
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Users loading error");
+      });
+  }, []);
+
   return (
     <div className="App">
       <Users />
